0a2d1c0b84f87c6d7533bac5abdbde6a5d7d7574
yanichik <brunshte@gmail.com>
2021-04-20 22:41:58 -0700

    set port

--- app.js
+++ app.js
@@ -32,6 +32,8 @@ const User = require('./models/user');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
 const MongoStore = require('connect-mongo');
+const port = process.env.PORT || 3000;
+
 
 // const dbURL = process.env.ATLAS_URL;
 // const dbURL = 'mongodb://localhost:27017/yelp-camp';
@@ -180,8 +182,8 @@ app.use((err, req, res, next) => {
 	res.status(statusCode).render('error', {err});
 })
 
-app.listen(3000, () => {
-	console.log('Listening to YelpCamp on port 3000');
+app.listen(port, () => {
+	console.log(`Listening to YelpCamp on port ${port}`);
 })
 
 
